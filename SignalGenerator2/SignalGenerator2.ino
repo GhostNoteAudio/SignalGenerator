eccc0b54db9f72b846a8988d6059f9811dcec97d
Ghost Note Audio <94485822+GhostNoteAudio@users.noreply.github.com>
2025-01-23 22:11:55 +0000

    sine wave out

--- SignalGenerator2/SignalGenerator2.ino
+++ SignalGenerator2/SignalGenerator2.ino
@@ -28,11 +28,13 @@ float scaler = 1.0f;
 
 AudioControlTLV320AIC3204 codec;
 
-const int32_t frequency = 1000;
-const int32_t amplitude = 1000000000; 
-const int32_t sampleRate = 48000;
-int32_t sample = amplitude; // current sample value
-int32_t count = 0;
+const float inc = 2 * M_PI * 1000.0 / 48000.0;
+const int32_t samplerate = 48000;
+const float ITERMAX = 2 * M_PI;
+float iter = 0.0f;
+volatile float amplitude = 1e9;
+bool noiseMode = false;
+const float RAND_SCALER = (float)(2.0 / RAND_MAX);
 
 void setup() 
 {
@@ -69,7 +71,7 @@ void setup()
     i2s.setBitsPerSample(32);
 
     // start I2S at the sample rate with 16-bits per sample
-    if (!i2s.begin(sampleRate)) 
+    if (!i2s.begin(samplerate)) 
     {
         Serial.println("Failed to initialize I2S!");
         while (1); // do nothing
@@ -87,6 +89,56 @@ void setup1()
     delay(1000);
 }
 
+int slideCounters[3] = {0};
+int activeCounter = 0;
+
+void updateSlideCounters()
+{
+    auto s0 = digitalRead(SLIDE1);
+    auto s1 = digitalRead(SLIDE2);
+    auto s2 = digitalRead(SLIDE3);
+
+    if (s0)
+    {
+      slideCounters[0]++;
+      slideCounters[1]--;
+      slideCounters[2]--;
+      if (slideCounters[0] > 10) slideCounters[0] = 10;
+      if (slideCounters[1] < 0) slideCounters[1] = 0;
+      if (slideCounters[2] < 0) slideCounters[2] = 0;
+    }
+    else if (s1)
+    {
+      slideCounters[0]--;
+      slideCounters[1]++;
+      slideCounters[2]--;
+      if (slideCounters[0] < 0) slideCounters[0] = 0;
+      if (slideCounters[1] > 10) slideCounters[1] = 10;
+      if (slideCounters[2] < 0) slideCounters[2] = 0;
+    }
+    else if (s2)
+    {
+      slideCounters[0]--;
+      slideCounters[1]--;
+      slideCounters[2]++;
+      if (slideCounters[0] < 0) slideCounters[0] = 0;
+      if (slideCounters[1] < 0) slideCounters[1] = 0;
+      if (slideCounters[2] > 10) slideCounters[2] = 10;
+    }
+    else
+    {
+      slideCounters[0]--;
+      slideCounters[1]--;
+      slideCounters[2]--;
+      if (slideCounters[0] < 0) slideCounters[0] = 0;
+      if (slideCounters[1] < 0) slideCounters[1] = 0;
+      if (slideCounters[2] < 0) slideCounters[2] = 0;
+    }
+
+    if (slideCounters[0] == 10) activeCounter = 0;
+    if (slideCounters[1] == 10) activeCounter = 1;
+    if (slideCounters[2] == 10) activeCounter = 2;
+}
 
 void loop1()
 {
@@ -97,30 +149,35 @@ void loop1()
     else
         newScaler = trimValue * 0.0011111111111111f; // / 900;
 
-    //scaler = scaler * 0.999f + newScaler * 0.001f;
-    scaler = newScaler;
-    Serial.print("Scaler value: ");
-    Serial.println(scaler);
+    scaler = scaler * 0.99f + newScaler * 0.01f;
+    updateSlideCounters();
+    
+    float slider = 0.0f;
+    if (activeCounter == 2) slider = 1.391e9;
+    else if (activeCounter == 1) slider = 0.795e9;
+    else if (activeCounter == 0) slider = 0.3565e9;
 
-    /*auto regResult = codec.readRegister(0x05, 0);
-    Serial.print("Codec register: ");
-    Serial.println(regResult, 2); // 0b11000100
-    */
-    delay(1000);
+    amplitude = slider * scaler;
+    delay(1);
 }
 
 
 void loop()
 {
-    if (count % 100 == 0) 
+    float s;
+
+    if (noiseMode)
     {
-        sample = -sample;
+        s = (rand() * RAND_SCALER - 1) * amplitude;
     }
-
+    else
+    {
+        s = sinf(iter) * amplitude;
+        iter += inc;
+        if (iter >= ITERMAX) 
+          iter -= ITERMAX;
+    }
     // write the same sample twice, once for left and once for the right channel
-    i2s.write(sample);
-    i2s.write(sample);
-
-    // increment the counter for the next sample
-    count++;
+    i2s.write((int32_t)s);
+    i2s.write((int32_t)s);
 }
